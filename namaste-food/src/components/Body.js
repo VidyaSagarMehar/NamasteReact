543ddb058a00a3b7f4882e06c44701210a178d23
VidyaSagarMehar <vidyasagark890@gmail.com>
2023-05-08 17:47:33 +0530

    Mock data imported from utils

--- namaste-food/src/components/Body.js
+++ namaste-food/src/components/Body.js
@@ -3,42 +3,7 @@ import RestaurantCard from './RestaurantCard';
 import { useState } from 'react';
 
 const Body = () => {
-	const [restaurantData, setRestaurantData] = useState([
-		{
-			info: {
-				id: '41095',
-				name: 'Nandhini Deluxe',
-				cloudinaryImageId: 'ylmpwj2vbvfqsiyspfd7',
-				locality: 'Gandhi Nagar',
-				areaName: 'Taj Mahal Building',
-				cuisines: [
-					'Andhra',
-					'Biryani',
-					'Chinese',
-					'North Indian',
-					'South Indian',
-				],
-				avgRating: 3.1,
-			},
-		},
-		{
-			info: {
-				id: '41096',
-				name: 'KFC',
-				cloudinaryImageId: 'ylmpwj2vbvfqsiyspfd7',
-				locality: 'Gandhi Nagar',
-				areaName: 'Taj Mahal Building',
-				cuisines: [
-					'Andhra',
-					'Biryani',
-					'Chinese',
-					'North Indian',
-					'South Indian',
-				],
-				avgRating: 4.1,
-			},
-		},
-	]);
+	const [restaurantData, setRestaurantData] = useState(resList);
 	return (
 		<div className="body">
 			<div className="search">
